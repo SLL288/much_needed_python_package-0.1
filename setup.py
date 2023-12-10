889e8d63fb31a754ae56609f86214fe2a9a8b64b
SLL28 <shaolun28@gmail.com>
2023-12-10 14:20:50 -0800

    aa

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+  
+setup(
+    name='much_needed_python_package',
+    version='0.1',
+    description='...',
+    author='sl0wc0der',
+    author_email='sl0wc0der@fastnation.com',
+    packages=['much_needed_python_package'],
+    install_requires=[],
+)
